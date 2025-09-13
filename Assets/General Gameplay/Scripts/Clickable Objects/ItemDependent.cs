b8e2fbf0b59a558f861de4bc5d867a98639f394f
LlocoGames <75390171+LlocoGames@users.noreply.github.com>
2025-09-12 22:34:56 -0600

    Shop UI

--- Assets/General Gameplay/Scripts/Clickable Objects/ItemDependent.cs
+++ Assets/General Gameplay/Scripts/Clickable Objects/ItemDependent.cs
@@ -1,31 +1,23 @@
 using Cinematics;
 using Player;
 using Player.Inventory;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
-public class ItemDependent : DescriptionObject
+public class ItemDependent : DescriptionObject 
 {
-    [SerializeField] private Player.Inventory.Object[] neededObjects;
-    HashSet<Player.Inventory.Object> neededSet;
+    Dialog lockedDialog;
 
-    private void Awake()
-    {
-        neededSet = new HashSet<Player.Inventory.Object>(neededObjects);
-    }
+    [Space(5)]
+    [SerializeField] private Player.Inventory.Object neededObject;
+
+    [Space(5)]
+    [SerializeField] UnityEvent onUnlock;
 
     public override void OnInteractStart()
     {
-        var selected = InteractionController.Instance.ItemSelected;
-        if (selected && neededSet.Contains(selected))
-        {
-            // Open door
-        } else
-        {
-
-            var a = DialogBoxController.IsDialogRunning?.Invoke();
-            if (a != null && !a.Value)
-                DialogBoxController.PlayDialog?.Invoke(dialog);
-        }
+        var a = DialogBoxController.IsDialogRunning?.Invoke();
+        if (a != null && !a.Value)
+            DialogBoxController.PlayDialog?.Invoke(dialog);
     }
 }
