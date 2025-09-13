c98604ca5568dd19a5360451cfb1bb30278b7400
LlocoGames <75390171+LlocoGames@users.noreply.github.com>
2025-09-12 22:34:56 -0600

    Shop UI

--- Assets/General Gameplay/Scripts/Clickable Objects/ItemDependent.cs
+++ Assets/General Gameplay/Scripts/Clickable Objects/ItemDependent.cs
@@ -2,10 +2,17 @@ using Cinematics;
 using Player;
 using Player.Inventory;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class ItemDependent : DescriptionObject 
 {
+    Dialog lockedDialog;
+
+    [Space(5)]
     [SerializeField] private Player.Inventory.Object neededObject;
+
+    [Space(5)]
+    [SerializeField] UnityEvent onUnlock;
 
     public override void OnInteractStart()
     {
